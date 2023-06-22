b957234f2223c68c97250fb0d65b8c159c4a6e18
Stefan Matuszynski <stefan.matuszynski-extern@swisslife.de>
2023-06-22 13:03:12 +0200

    added settings.xml

--- book-store-server/mvnw.cmd
+++ book-store-server/mvnw.cmd
@@ -139,7 +139,7 @@ if exist %WRAPPER_JAR% (
 )
 @REM End of extension
 
-%MAVEN_JAVA_EXE% %JVM_CONFIG_MAVEN_PROPS% %MAVEN_OPTS% %MAVEN_DEBUG_OPTS% -classpath %WRAPPER_JAR% "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" %WRAPPER_LAUNCHER% %MAVEN_CONFIG% %*
+%MAVEN_JAVA_EXE% %JVM_CONFIG_MAVEN_PROPS% %MAVEN_OPTS% %MAVEN_DEBUG_OPTS% -classpath %WRAPPER_JAR% "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" %WRAPPER_LAUNCHER% %MAVEN_CONFIG% -s settings.xml %*
 if ERRORLEVEL 1 goto error
 goto end
 
