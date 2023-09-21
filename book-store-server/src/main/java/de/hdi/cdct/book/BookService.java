b1e42740df8dc5770d2348681715a31def1f0aba
Stefan Matuszynski <stefan.matuszynski@pirobase-imperia.com>
2023-09-21 08:23:29 +0200

    changes

--- book-store-server/src/main/java/de/hdi/cdct/book/BookService.java
+++ book-store-server/src/main/java/de/hdi/cdct/book/BookService.java
@@ -1,27 +1,26 @@
 package de.hdi.cdct.book;
 
-import com.github.javafaker.Faker;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
+import java.util.Map;
 
 @Service
 public class BookService {
+  private final Map<String, Book> books = Collections.synchronizedMap(new HashMap<>());
+
+  public BookService() {
+      Book book = new Book();
+      book.setGenre("Technology");
+      book.setTitle("Java 11");
+      book.setIsbn("42");
+      books.put(book.getIsbn(), book);
+  }
 
   public List<Book> getBooks() {
-    Faker faker = new Faker();
-    List<Book> books = new ArrayList();
-
-    for (int i = 0; i < 10; i++) {
-      Book book = new Book();
-      book.setGenre(faker.book().genre());
-      book.setTitle(faker.book().title());
-      book.setIsbn(UUID.randomUUID().toString());
-      books.add(book);
-    }
-
-    return books;
+    return new ArrayList<>(books.values());
   }
 }
